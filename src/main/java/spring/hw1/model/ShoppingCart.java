fd99bdbfc45fc2f9e8cf7251434a53e97433f132
sergeyzelentsov <_sharapov_@mail.ru>
2022-02-03 16:38:01 +0300

    HW1

---
+++ src/main/java/spring/hw1/model/ShoppingCart.java
@@ -0,0 +1,15 @@
+package spring.hw1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class ShoppingCart {
+    private List<Product> products;
+}
---
+++ src/main/webapp/product.jsp
@@ -0,0 +1,9 @@
+<%--<html>--%>
+<%--    <head>product</head>--%>
+<%--<body>--%>
+<%--    <h2>product</h2>--%>
+<%--    <p>id: ${product.id}</p>--%>
+<%--    <p>title:  ${product.title}</p>--%>
+<%--    <p>cost: ${product.cost}</p>--%>
+<%--</body>--%>
+<%--</html>--%>
